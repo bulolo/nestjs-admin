1f61b9dae69e7e808b32ab472e14f32985beb184
俊瑶 <82607314@qq.com>
2021-03-25 17:10:11 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,6 @@
+import { AuthModule } from './system/auth/auth.module';
+import { AuthService } from './system/auth/auth.service';
+import { AuthController } from './system/auth/auth.controller';
 import { UserModule } from './system/user/user.module';
 import { UserService } from './system/user/user.service';
 import { UserController } from './system/user/user.controller';
@@ -11,12 +14,15 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
+    AuthModule,
     UserModule,
     RoleModule],
   controllers: [
+    AuthController,
     UserController,
     RoleController, AppController],
   providers: [
+    AuthService,
     UserService,
     RoleService, AppService],
 })
