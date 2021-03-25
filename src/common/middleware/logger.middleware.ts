a642af137adb48c6a1d6996e3ce4829b1dd38340
俊瑶 <82607314@qq.com>
2021-03-25 16:57:44 +0800

    增加中间件,增加Swagger文档

---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,19 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: Function) {
+    const logFormat = `  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      Request original url: ${req.originalUrl}
+      Method: ${req.method}
+      IP: ${req.ip}
+      Status code: ${res.statusCode}
+      Parmas: ${JSON.stringify(req.params)}
+      Query: ${JSON.stringify(req.query)}
+      Body: ${JSON.stringify(req.body)} \n  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 
+    `
+    console.log(logFormat);
+    next();
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  // swagger文档
+  const config = new DocumentBuilder()
+    .setTitle('race-nestjs-admin')
+    .setDescription('nestjs-admin 基于RABC的通用api文档')
+    .setVersion('1.0')
+    .addTag('通用api接口')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+
+  app.use(new LoggerMiddleware().use)
+
   await app.listen(3000);
 }
 bootstrap();
