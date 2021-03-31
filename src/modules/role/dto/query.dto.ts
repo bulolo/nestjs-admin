3e1e44e80f84d916f368b1cd9dfe84f9d40433e6
俊瑶 <82607314@qq.com>
2021-03-31 21:35:28 +0800

    update

--- src/modules/role/dto/query.dto.ts
+++ src/modules/role/dto/query.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsMobilePhone, IsNotEmpty, IsNumber, IsOptional, IsPhoneNumber, IsString, MaxLength, MinLength } from 'class-validator'
+import { BaseQueryDto } from 'src/modules/_base/baseQuery.dto'
 
-export class QueryRoleDto {
+export class QueryRoleDto extends BaseQueryDto{
 
 }
