8da3a475b6305c99cc347ba6c3ca68610d57c9a8
俊瑶 <82607314@qq.com>
2021-05-12 13:27:38 +0800

    增加若干中间件

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
+import { AppModule } from './app.module'
 import { LoggerMiddleware } from './common/middleware/logger.middleware'
 import { ConfigService } from "@nestjs/config"
 import { BadRequestException, Logger, ValidationError, ValidationPipe } from '@nestjs/common'
 import * as express from 'express'
-import { AllExceptionsFilter } from './common/exception/all-exception.filter';
+import * as compression from 'compression'
+import * as rateLimit from 'express-rate-limit'
+import * as csurf from 'csurf'
+import * as helmet from 'helmet';
+import { AllExceptionsFilter } from './common/exception/all-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -13,17 +17,27 @@ async function bootstrap() {
   const config = app.get(ConfigService)
 
   //允许跨域
-  app.enableCors();
+  app.enableCors()
 
-
-
+  // gzip压缩
+  app.use(compression())
   // For parsing application/json
   app.use(express.json())
   // For parsing application/x-www-form-urlencoded
   app.use(express.urlencoded({ extended: true }))
+  // 速率限制
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 1000, // limit each IP to 100 requests per windowMs
+    }),
+  )
+  // CSRF保护
+  app.use(csurf())
+  // 漏洞保护
+  app.use(helmet());
   // 日志中间件
   // app.use(new LoggerMiddleware().use)
-
 
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
@@ -31,14 +45,14 @@ async function bootstrap() {
       enableImplicitConversion: true
     },
     // exceptionFactory: (errors: ValidationError[]) => new BadRequestException('参数校验错误')
-  }));
+  }))
 
   // 所有异常
   app.useGlobalFilters(new AllExceptionsFilter())
 
   // 设置 api 访问前缀
   app.setGlobalPrefix('/api')
-  
+
   // swagger文档
   if (config.get<boolean>('app.swagger')) {
     const swaggerOptions = new DocumentBuilder()
@@ -47,13 +61,13 @@ async function bootstrap() {
       .setVersion('1.0')
       // .addTag('通用api接口')
       .addBearerAuth({ in: 'header', type: 'http' })
-      .build();
-    const document = SwaggerModule.createDocument(app, swaggerOptions);
-    SwaggerModule.setup('docs', app, document);
+      .build()
+    const document = SwaggerModule.createDocument(app, swaggerOptions)
+    SwaggerModule.setup('docs', app, document)
     Logger.log(`/docs`, 'swagger启动成功')
   }
 
-  await app.listen(config.get<number>('app.port') || 8080);
+  await app.listen(config.get<number>('app.port') || 8080)
 
   const appLocalPath = await app.getUrl()
 
@@ -61,4 +75,4 @@ async function bootstrap() {
 
   Logger.log(process.env.NODE_ENV, '当前启动环境')
 }
-bootstrap();
+bootstrap()
